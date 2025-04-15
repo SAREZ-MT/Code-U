58aeaf44129793789f09c11359b71ab9200a3e5a
Javier Sanguino <jsanguino@estudiante.uniajc.edu.co>
2025-04-15 12:00:29 -0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBdO7bod5H6K_Ql9TyZOVDxLvdLtxpdJIM",
-  authDomain: "code-u-814ef.firebaseapp.com",
-  projectId: "code-u-814ef",
-  storageBucket: "code-u-814ef.firebasestorage.app",
-  messagingSenderId: "929869917674",
-  appId: "1:929869917674:web:031e7d204abe7d5eb81693"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Inicializar Firebase
